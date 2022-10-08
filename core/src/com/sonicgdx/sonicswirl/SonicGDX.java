09e76f49820aa39d2d05d5accac7498202c5f0d4
NylonLogic1355 <114670430+NylonLogic1355@users.noreply.github.com>
2022-10-08 20:04:14 +0100

    Changed core/build.gradle sourceCompatibility to 1.8, renamed square and added early shape renderer

--- core/src/com/sonicgdx/sonicswirl/SonicGDX.java
+++ core/src/com/sonicgdx/sonicswirl/SonicGDX.java
@@ -3,6 +3,7 @@ package com.sonicgdx.sonicswirl;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.math.Rectangle;
 
@@ -10,18 +11,24 @@ public class SonicGDX extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	Rectangle floor;
+
+	ShapeRenderer debug;
+
 	// Rectangle used in gdx.math is different to the rectangle in java.awt.Rectangle
 
 	// https://libgdx.com/wiki/start/a-simple-game
 	@Override
 	public void create () { // equivalent to start in unity
 		batch = new SpriteBatch();
-		img = new Texture("square.jpg");
+		img = new Texture("square-16.png");
 		floor = new Rectangle();
+		debug = new ShapeRenderer();
 		floor.x = 800 / 2 - 640 / 2;
 		floor.y = 20;
 		floor.width = 10;
 		floor.height = 10;
+		// size of the rectangle isn't the same as the size of the texture so changing the width
+		// and height of the rectangle does not scale the texture.
 	}
 
 	@Override
@@ -29,7 +36,13 @@ public class SonicGDX extends ApplicationAdapter {
 		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
 		batch.draw(img, 250, 250, 1, 1, 50, 50); // draw the square texture
+
 		batch.draw(img,floor.x,floor.y); //draw the rectangle
+
+		// https://stackoverflow.com/a/43751578
+		debug.begin();
+		debug.rect(floor.x,floor.y,floor.width,floor.height);
+		debug.end();
 
 		/*batch.draw(img, 0, 0, 1,1,1000,100);
 		for (int i=0; i< 5; i++){
