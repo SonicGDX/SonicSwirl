ad7e602fe0c36027773c11606fbff49e1b413248
SonicGDX <114670430+SonicGDX@users.noreply.github.com>
2022-10-17 10:13:56 +0100

    Use short form for some if statements (inspiration taken from render method in supertype)
    See also ternary operator: https://stackoverflow.com/questions/8898590/short-form-for-java-if-statement

--- core/src/com/sonicgdx/sonicswirl/SonicGDX.java
+++ core/src/com/sonicgdx/sonicswirl/SonicGDX.java
@@ -15,9 +15,9 @@ public class SonicGDX extends Game {
 	Texture img;
 	OrthographicCamera camera;
 
-	float speed;
+	float speed = 0;
 
-	int x = 1; //https://colourtann.github.io/HelloLibgdx/
+	int x = 100; //https://colourtann.github.io/HelloLibgdx/
 	int y = 10;
 
 	// https://libgdx.com/wiki/start/a-simple-game
@@ -25,7 +25,6 @@ public class SonicGDX extends Game {
 	public void create () { // equivalent to start in unity
 
 		// implement class with reference to https://gamedev.stackexchange.com/a/133593
-
 
 		camera = new OrthographicCamera(); // 3D camera which projects into 2D.
    		camera.setToOrtho(false, 1280, 720); // Even if the device has a scaled resolution, the in game view will still be 1280x720
@@ -52,14 +51,10 @@ public class SonicGDX extends Game {
 
 		// capital F can be used to cast from double to float (e.g. 50.55F)
 
-		/*if (250-y >= 125)
-		{
-			y += 10;
-		}*/
+		//if (250-y >= 125) y += 10;
 
-		if (speed <= 6){
-			speed += 0.046875;
-		}
+		if (speed <= 6) speed += 0.02;
+		//speed += 0.046875;
 
 		if (x<=1280){
 			x+=speed;
