93a3e28bb2a549715d6a69e366e80fa7c479e408
SonicSwirl <114670430+SonicGDX@users.noreply.github.com>
2022-10-28 22:33:39 +0100

    Clean up code

--- core/src/com/sonicgdx/sonicswirl/SonicGDX.java
+++ core/src/com/sonicgdx/sonicswirl/SonicGDX.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.Game;
 // input handling:
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -20,7 +22,7 @@ public class SonicGDX extends Game {
 	OrthographicCamera camera;
 	// capital F can be used to cast from double to float (e.g. 50.55F)
 	float speedX = 0, speedY = 0, groundSpeed = 0;
-	float x = 25, y = 100;; //https://colourtann.github.io/HelloLibgdx/
+	float x = 600, y = 200;; //https://colourtann.github.io/HelloLibgdx/
 
 
 	//https://info.sonicretro.org/SPG:Solid_Tiles#Sensors
@@ -62,14 +64,16 @@ public class SonicGDX extends Game {
 	@Override
 	public void render () { // equivalent to update in unity
 
-		ScreenUtils.clear(1, 0.2F, 0, 1); // clears the screen and sets the background to a certain colour
+		//OLD ScreenUtils.clear(1, 0.2F, 0, 1);
+		ScreenUtils.clear(Color.GRAY); // clears the screen and sets the background to a certain colour
+
 
 		camera.update(); // recompute matrix for orthographical projection - this is necessary if it needs to move.
 
-		if (!isGrounded)
+		/*if (!isGrounded)
 		{
 			y -= 9.8;
-		}
+		}*/
 
 		//if (250-y >= 150) y += 10;
 		if (Gdx.input.isKeyPressed(Input.Keys.D)) {
@@ -90,17 +94,13 @@ public class SonicGDX extends Game {
 		if (x < 1280) System.out.println("Ground Speed = " + groundSpeed + " & x = " + x);
 
 		leftFootSensor = sprite1.getX();
-		rightFootSensor = sprite1.getX() + (25 - 1); // x pos + (srcWidth - 1) - using srcWidth places it one pixel right of the square
+		rightFootSensor = sprite1.getX() + (sprite1.getWidth() - 1); // x pos + (srcWidth - 1) - using srcWidth places it one pixel right of the square
 
 
 		// tell the SpriteBatch to render in the
 		// coordinate system specified by the camera. - https://libgdx.com/wiki/start/a-simple-game
 		batch.setProjectionMatrix(camera.combined);
-
 		batch.begin();
-
-
-
 			// Individual tile
 			for (int block = 0; block < heightArray2.length; block++) {
 				// Individual block
@@ -108,18 +108,11 @@ public class SonicGDX extends Game {
 					batch.draw(img, block + 100, 100 + h);
 				}
 			}
-
-		sprite1.draw(batch);
-
-		//System.out.println(sprite1.getBoundingRectangle()); // use for collision detection - to set, it is sprite.SetBounds()
-
-		batch.draw(img2,0,0,1240,100); //draw the floor
-
-		batch.draw(img,leftFootSensor,y); // draw left foot sensor - DEBUG
-		batch.draw(img,rightFootSensor,y); // draw right foot sensor - DEBUG
-
-
-
+			sprite1.draw(batch);
+			//System.out.println(sprite1.getBoundingRectangle()); // use for collision detection - to set, it is sprite.SetBounds()
+			batch.draw(img2,0,0,1240,100); //draw the floor
+			batch.draw(img,leftFootSensor,y); // draw left foot sensor - DEBUG
+			batch.draw(img,rightFootSensor,y); // draw right foot sensor - DEBUG
 		batch.end();
 	}
 	
