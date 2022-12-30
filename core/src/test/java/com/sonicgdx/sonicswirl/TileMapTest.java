31a04fe1ff32cc9195a0daa505938a5ba99781f0
SonicGDX <114670430+SonicGDX@users.noreply.github.com>
2022-12-30 23:56:21 +0000

    Use reflection

--- core/src/test/java/com/sonicgdx/sonicswirl/TileMapTest.java
+++ core/src/test/java/com/sonicgdx/sonicswirl/TileMapTest.java
@@ -1,6 +1,8 @@
 package com.sonicgdx.sonicswirl;
 
+import com.badlogic.gdx.utils.reflect.Annotation;
 import com.badlogic.gdx.utils.reflect.ClassReflection;
+import com.badlogic.gdx.utils.reflect.Field;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -12,6 +14,16 @@ class TileMapTest {
     void checkArrayLengths()
     {
         System.out.println(Arrays.toString(ClassReflection.getFields(TileMap.class)));
+
+        for (Field field:ClassReflection.getDeclaredFields(TileMap.class)) {
+            for (Annotation annotation : field.getDeclaredAnnotations()) {
+                System.out.println(annotation.getAnnotationType());
+            }
+            /*if (field.getType() == byte[])
+            {
+
+            }*/
+        }
     }
 
 }
