f9a7dab8f4a0fb4ab9df96a9b989f8d4361f5f64
SonicGDX <114670430+SonicGDX@users.noreply.github.com>
2022-12-31 14:31:37 +0000

    Fixed by moving batch initialisation to before MenuScreen constructor is called.

--- core/src/main/java/com/sonicgdx/sonicswirl/Init.java
+++ core/src/main/java/com/sonicgdx/sonicswirl/Init.java
@@ -12,9 +12,9 @@ public class Init extends Game {
 	private MenuScreen menuScreen;
 
 	public void create() {
-		menuScreen = new MenuScreen(this);
-		batch = new SpriteBatch(); // sprite batch provides multiple sprites to draw to the GPU to improve openGl performance https://gamedev.stackexchange.com/questions/32910/what-is-the-technical-defInition-of-sprite-batching
+        batch = new SpriteBatch(); // sprite batch provides multiple sprites to draw to the GPU to improve openGl performance https://gamedev.stackexchange.com/questions/32910/what-is-the-technical-defInition-of-sprite-batching
         // Can be easily shared between screens
+		menuScreen = new MenuScreen(this);
 		font = new BitmapFont(); // Default font = Liberation Sans
 		this.setScreen(menuScreen);
 	}
