17141ac1fa9804917e4c106d419e89f953256440
SonicGDX <114670430+SonicGDX@users.noreply.github.com>
2023-01-01 17:16:00 +0000

    Move downSensorCheck to FloorSensor

--- core/src/main/java/com/sonicgdx/sonicswirl/FloorSensor.java
+++ core/src/main/java/com/sonicgdx/sonicswirl/FloorSensor.java
@@ -1,24 +1,98 @@
 package com.sonicgdx.sonicswirl;
 
+import com.badlogic.gdx.math.MathUtils;
+
 public class FloorSensor implements Sensor{
 
-    private boolean active;
-    private float xPos, yPos;
+    private boolean isActive;
+    private float xPosition, yPosition;
 
     private Tile tile;
     private float distance;
 
     public FloorSensor(float xPos, float yPos) {
-        this.xPos = xPos;
-        this.yPos = yPos;
+        this.xPosition = xPos;
+        this.yPosition = yPos;
     }
 
     @Override
-    public void check() {
+    public void process() {
+        if (xPosition < 0 || yPosition < 0) {
+            tile = TileMap.getEmpty(); distance = -50;
+        }
+        //TODO prevent catch block in getTile() from being used.
 
+        int tileX = Math.floorMod(MathUtils.round(xPosition), 128) / 16;
+        int chunkX = MathUtils.round(xPosition) / 128;
+
+        int tileY = Math.floorMod(MathUtils.round(yPosition), 128) / 16;
+        int chunkY = MathUtils.round(yPosition) / 128;
+
+        int block = Math.floorMod(MathUtils.round(xPosition),16); //Different behaviour for negative numbers compared to using %. For
+        // example, -129 % 16 would return -1 which would cause an ArrayIndexOutOfBoundsException. Math.floorMod() would return a positive index in these cases.
+
+        float distance = 0;
+        byte height;
+
+        int tempTileY, tempChunkY;
+
+        height = TileMap.getTile(chunkX,chunkY,tileX,tileY).getHeight(block);
+
+        distance = ((chunkY * 128) + (tileY * 16) + height) - yPosition;
+
+        if (height == 16)
+        {
+            // sensor regression, checks one tile above with downwards facing sensors in an attempt to find surface if the height of the array is full
+            if (tileY < 7)
+            {
+                tempChunkY = chunkY;
+                tempTileY = tileY + 1;
+            }
+            else
+            {
+                tempChunkY = chunkY + 1;
+                tempTileY = 0;
+            }
+
+            height = TileMap.getTile(chunkX,tempChunkY,tileX,tempTileY).getHeight(block);
+            if (height > 0) //TODO outline conditions in comment
+            {
+                chunkY = tempChunkY;
+                tileY = tempTileY;
+
+                distance += height;
+            }
+        }
+
+        else if (height == 0)
+        {
+            // sensor extension, checks one tile below with downwards facing sensors in an attempt to find surface
+            if (tileY == 0)
+            {
+                chunkY--;
+                tileY = 7;
+            }
+            else tileY--;
+
+            height = TileMap.getTile(chunkX,chunkY,tileX,tileY).getHeight(block);
+
+            if (height == 0) distance -= 16;
+            else distance -= (16-height);
+        }
+        tile = TileMap.getEmpty(); distance = -50;
     }
 
     public void setPosition(float x, float y) {
-        xPos = x; yPos = y;
+        xPosition = x; yPosition = y;
     }
+    public float getDistance() {
+        return distance;
+    }
+    public Tile getTile() {
+        return tile;
+    }
+    public boolean getActive() {
+        return isActive;
+    }
+
 }
--- core/src/main/java/com/sonicgdx/sonicswirl/Sensor.java
+++ core/src/main/java/com/sonicgdx/sonicswirl/Sensor.java
@@ -1,6 +1,6 @@
 package com.sonicgdx.sonicswirl;
 
 public interface Sensor {
-    void check();
+    void process();
 
 }
